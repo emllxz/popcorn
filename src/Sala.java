717ed0c75da473fafe6614052d04e8aa2b7adeff
2222000271 <diasemillim2006@gmail.com>
2026-06-16 20:33:37 -0300

    Inicio de Assento ainda com problemas

--- src/Sala.java
+++ src/Sala.java
@@ -1,13 +1,36 @@
 public class Sala {
 
+    public enum TipoSala{
+        PEQUENA(5, 8),
+        MEDIA(8, 12),
+        GRANDE(12, 20);
+
+        private final int filas;
+        private final int colunas;
+
+        TipoSala(int filas, int colunas){
+            this.filas = filas;
+            this.colunas = colunas;
+        }
+
+        public int getFilas(){
+            return filas;
+        }
+
+        public int getColunas(){
+            return colunas;
+        }
+    }
+
     private int idSala;
     public int numeroSala;
-    protected int capacidade;
 
-    public Sala(int idSala, int numeroSala, int capacidade){
+    private TipoSala tipoSala;
+
+    public Sala(int idSala, int numeroSala, TipoSala tipoSala){
         this.idSala = idSala;
         this.numeroSala = numeroSala;
-        this.capacidade = capacidade;
+        this.tipoSala = tipoSala;
     }
 
     //GETTERS E SETTERS
@@ -17,15 +40,14 @@ public class Sala {
     public int getNumeroSala(){return numeroSala; }
     public void setNumeroSala(int numeroSala){this.numeroSala = numeroSala; }
 
-    public int getCapacidade(){return capacidade; }
-    public void setCapacidade(int capacidade) {this.capacidade = capacidade; }
+    public TipoSala getTipoSala(){return tipoSala;}
+
 
     @Override
     public String toString(){
         return "\n----- SALA -----" +
                "\nId: " + idSala +
-               "\nNúmero da Sala: " + numeroSala +
-               "\nCapacidade de Pessoas: " + capacidade;
+               "\nNúmero da Sala: " + numeroSala;
     }
 }
 
